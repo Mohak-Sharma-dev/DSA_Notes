4961b4e8b4c3fcc9624b5e1e9d0eae99562b8492
Mohak-Sharma-dev <mohaksharma34@gmail.com>
2025-03-16 12:52:57 +0500

    Lec2

---
+++ Lec_1_First_Prog/Tasks.java
@@ -0,0 +1,73 @@
+
+
+import java.util.Scanner;
+
+public class Tasks {
+    /*
+     * Learn For, While, Do-While and For-Each
+     */
+    public static void main(String[] args) {
+        /*Largest of the three numbers.
+         * input three numbers then compare
+         * 
+         */
+        Scanner input = new Scanner(System.in);
+
+        // int a,b,n;
+
+        // a = input.nextInt();
+        // b = input.nextInt();
+        // n = input.nextInt();
+
+        // input.close();
+
+        // int max = a;
+
+        // if (b > max) {
+        //     max = b;
+        // }
+        // if(n > max)
+        // {
+        //     max = n;            
+        // }
+        // System.out.println(max);
+
+        //CASE check
+
+        // char ch = input.next().trim().charAt(0);
+        // /*
+        //  * .next(): Takes the string
+        //  * .trim(): Takes/removes extra blank spaces
+        //  * .charAt(0): Takes string at 0 index.
+        //  */
+
+        // // System.out.println(ch);
+
+        // if (ch >= 'a' && ch <= 'z') {
+        //     System.out.println("Lowercase");
+        // }else if (ch <= 'A' && ch >= 'Z') {
+        //     System.out.println("Uppercase");
+
+        // }else {
+        //     System.out.println("Neither");
+        // }
+
+
+        //Fibonacci sequence
+
+        int prev = 0;
+        int init = 1;
+        int n;
+        int count = 2;
+
+        n = input.nextInt();
+
+        while (count <= n)
+        {
+
+        }
+
+
+
+    }
+}
