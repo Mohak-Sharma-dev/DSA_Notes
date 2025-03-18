a4718173042656e207aedb2bcae463e2e627823b
Mohak <mohaksharma34@gmail.com>
2025-03-18 13:07:26 +0500

    Reverse

--- Lec_2_Conditional_loops/Reverse_num.java
+++ Lec_2_Conditional_loops/Reverse_num.java
@@ -4,16 +4,42 @@ import java.util.Scanner;
 
 public class Reverse_num {
     public static void main(String[] args) {
-        long a;
-        int b;
-        int c;
-        
+        Integer init;
+        long reverse=0;        
         Scanner input = new Scanner(System.in);
-        a = input.nextInt();
-        b = input.nextInt();
+        init = input.nextInt();
+        input.close();
 
-        while (a>0) {
-            
+        // String num1 = Integer.toString(init);
+
+        // int count = 0;
+
+        while (init > 0) {
+            int remainder = init % 10;
+            init /= 10;
+            reverse = reverse*10 + remainder;
+            // long rem = init%10;
+            // init = init/10;
+            // System.out.println(reverse);
+            // System.out.println("After division, init: "+init+"\n");
+            // if (reverse == 0 ) {
+            //     reverse = rem;
+            // }
+            // else{
+            //     reverse = rem*10 + rem;
+            // }
+            // System.out.println("After assigning to reverse, remainder is:"+rem+"\n");
+            // System.out.println("Which means reverse becomes:"+reverse+"\n");
+            // count++;
         }
+        System.out.println("Reverse finnaly is: "+reverse+"\n");
     }
 }
+
+
+
+/*
+ * 
+ * 
+ * 
+ */
